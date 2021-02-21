36f70efbfed6ce1f7839f7cc716f869ef9145c05
admin <jtobelem@simplon.co>
2021-02-21 12:21:28 +0100

    Saved recipe: LPSMODULE1COVID.compute_confirmed_pivoted

--- recipes/compute_confirmed_pivoted.py
+++ recipes/compute_confirmed_pivoted.py
@@ -4,11 +4,11 @@ import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
 
 # Read recipe inputs
-2_time_series_covid19_confirmed_global = dataiku.Dataset("2_time_series_covid19_confirmed_global")
-2_time_series_covid19_confirmed_global_df = 2_time_series_covid19_confirmed_global.get_dataframe()
+confirmed_global = dataiku.Dataset("2_time_series_covid19_confirmed_global")
+confirmed_global_df = confirmed_global.get_dataframe()
 
 
-confirmed_pivoted_df = 2_time_series_covid19_confirmed_global_df.melt(id_vars=["Country/Region", "Province/State", "Lat", "Long"], 
+confirmed_pivoted_df = confirmed_global_df.melt(id_vars=["Country/Region", "Province/State", "Lat", "Long"], 
         var_name="Date", 
         value_name="Value")
 
