02e463f8620038730c92ad2b32021d6c4585e3da
admin <jtobelem@simplon.co>
2021-02-21 12:33:05 +0100

    Saved recipe: LPSMODULE1COVID.compute_death_pivoted

--- recipes/compute_death_pivoted.py
+++ recipes/compute_death_pivoted.py
@@ -4,11 +4,11 @@ import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
 
 # Read recipe inputs
-time_series_covid19_deaths_global = dataiku.Dataset("time_series_covid19_deaths_global")
-deaths_global_df = time_series_covid19_deaths_global.get_dataframe()
+deaths = dataiku.Dataset("time_series_covid19_deaths_global")
+deaths_df = deaths.get_dataframe()
 
 
-death_pivoted_df = deaths_global_df.melt(id_vars=["Country/Region", "Province/State", "Lat", "Long"], 
+death_pivoted_df = deaths_df.melt(id_vars=["Country/Region", "Province/State", "Lat", "Long"], 
         var_name="Date", 
         value_name="Value")
 
